69320cd8a1cbfaee2b33c2b621433bf65df6caf4
vpelkonen <ville.pelkonen@qvik.fi>
2020-03-21 19:30:15 +0200

    Add links constants

---
+++ src/constants/links.js
@@ -0,0 +1,14 @@
+export default {
+  gitHub: {
+    url: 'https://github.com',
+    text: 'GitHub',
+  },
+  linkedIn: {
+    url: 'https://linkedin/',
+    text: 'LinkedIn',
+  },
+  qvik: {
+    url: 'https://qvik.com',
+    text: 'Qvik',
+  },
+}
