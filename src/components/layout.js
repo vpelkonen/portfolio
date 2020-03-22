41120164725512d3c2b02858ea8f7725c58d06b2
vpelkonen <ville.pelkonen@qvik.fi>
2020-03-22 12:40:47 +0200

    Clean unused refs, fix links

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,35 +1,15 @@
 import React from "react"
-import PropTypes from "prop-types"
 import styled from 'styled-components'
-import { useStaticQuery, graphql } from "gatsby"
 import GlobalStyles, { CSSReset } from '../theme/global'
 
 const Main = styled.main`
   max-width: 920px;
 `
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
-  return (
+export default ({ children }) => (
     <>
       <CSSReset />
       <GlobalStyles />
       <Main>{children}</Main>
     </>
-  )
-}
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
-
-export default Layout
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,17 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 import LINKS from '../constants/links'
-console.log(LINKS)
+
 export default () => (
   <Layout>
     <SEO title="Ville Pelkonen | Software Development & Consultancy" />
     <h1>Hi, I'm Ville Pelkonen!</h1>
     <p>I craft sites and apps for the web and beyond.</p>
     <p>Currently I work with <span>React</span> and <span>React Native</span>. I also like e.g. <span>TypeScript</span>, <span>Gatsby</span> and <span>Netlify</span>, and have 7+ years of experience from various other technologies. I have my CIs and CDs under control and know my way around Google and Apple.</p>
-    <p>At the moment I work as a <span>Senior Software Engineer</span> and <span>Web Tech Lead</span> at <Link to={LINKS.qvik.url}>{LINKS.qvik.text}</Link>.</p>
-    <Link to={LINKS.linkedIn.url}>{LINKS.linkedIn.text}</Link>
-    <Link to={LINKS.gitHub.url}>{LINKS.gitHub.text}</Link>
+    <p>At the moment I work as a <span>Senior Software Engineer</span> and <span>Web Tech Lead</span> at <a href={LINKS.qvik.url}>{LINKS.qvik.text}</a>.</p>
+    <a href={LINKS.linkedIn.url}>{LINKS.linkedIn.text}</a>
+    <a href={LINKS.gitHub.url}>{LINKS.gitHub.text}</a>
   </Layout>
 )
